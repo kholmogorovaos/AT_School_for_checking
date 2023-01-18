6eef5e7c05740fef4783a059ae5f0576c040c9f5
Kholmogorova Olga <oliya.holms@gmail.com>
2023-01-18 17:52:58 +0400

    ДЗ к уроку 2.31 Типы данных v2

--- Lesson 2.31/src/Program4.java
+++ Lesson 2.31/src/Program4.java
@@ -1,8 +1,8 @@
 public class Program4 {
     public static void main(String[] args) {
-        double P = Double.parseDouble(args[0]);
-        double r = Double.parseDouble(args[1]);
-        int t = Integer.parseInt(args[2]);
+       estimation(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Integer.parseInt(args[2]));
+    }
+    public static void estimation(double P, double r, int t) {
         double summa = (P * Math.exp(r/100*t));
         System.out.println("Задание 4:");
         System.out.printf("Дано: P = %s, ", P);
