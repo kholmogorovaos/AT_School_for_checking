916392525b0b38d0a6a5b593f1dca82004b87c26
Kholmogorova Olga <oliya.holms@gmail.com>
2023-02-10 10:07:16 +0400

    ДЗ к уроку 2.47 v2

--- Lesson 2.47/src/Task3.java
+++ Lesson 2.47/src/Task3.java
@@ -8,22 +8,27 @@ public class Task3 {
         } else System.out.println("Ошибка: неверно введены значения");
     }
         public void snakePrint(int n, int col) {
-        int n1 = n / col +  (n % col == 0 ? 0 : 1);
-        int matrix[][] = new int[n1][col];
-        for (int ik = 0; ik < n1; ik++)
-            for (int jk = 0; jk < col; jk++)
-                matrix[ik][jk] = 0;
-        for (int ik = 0; ik < n1; ik++) {
-            for (int jk = 0; jk < col; jk++) {
-                int i = ik + 1;
-                int j = jk + 1;
-                matrix[ik][jk] = ((i - 1) * col + j) * (i % 2) + (i * col + 1 - j) * ((i + 1) % 2);
+            int n1 = n/col; //+ (n % col == 0 ? 0 : 1);
+            int matrix[][] = new int[n1][col];
+            int value = 1;
+            for (int i = 0; i < n1; ++i) {
+                if (i % 2 == 0) {
+                    for (int j = 0; j < col; ++j) {
+                        matrix[i][j] = value;
+                        value++;
+                    }
+                }
+                else {
+                    for (int j = col-1; j >= 0; --j) {
+                        matrix[i][j] = value;
+                        value++;
+                    }
+                }
+            }
+            for (int i = 0; i < n1; ++i) {
+                for (int j = 0; j < col; ++j) {
+                        System.out.printf(matrix[i][j] + " ");
+                    } System.out.println();
             }
         }
-        for (int ik = 0; ik < n1; ik++) {
-            for (int jk = 0; jk < col; jk++) {
-                System.out.printf(" %02d  ", matrix[ik][jk]);
-            } System.out.println("\n");
-        }
-    }
 }
